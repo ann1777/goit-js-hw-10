d6fcb86276e9c24fb8e12a7b359c14081c22e75d
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-27 22:11:33 +0300

    Added import markupCountries to the index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './css/styles.css';
 import { fetchCountryInfo } from './fetchCountryInfo';
+import { renderCountryList, renderCountryInfo } from './markupCountries';
 import debounce from 'lodash.debounce';
+import {Notify} from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1300;
 
-const searchInput = document.querySelector('#input#search-box')
-const countryList = document.querySelector('country-list')
-const countryInfo = document.querySelector('country-info')
+const searchInput = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 searchInput.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
 
@@ -14,24 +16,29 @@ function onSearchInput(event) {
     event.preventDefault();
 
     const countryName = searchInput.value.trim();
-    if(countryName === "") {
+    if(!searchInput.value) {
         clearPageHtml();
         return;
     }
 
     fetchCountryInfo(countryName)
         .then(countries => {
-            clearPage();
+            clearPageHtml();
+
             if (countries.length > 10) {
                 alertTooManyMatchesFound();
             } else if(countries.length >= 2 && countries.length <= 10) {
                 countryList.insertAdjacentHTML("beforeend", renderCountryList(countries));
             } else {
                 countryList.insertAdjacentHTML('beforeend', renderCountryList(countries));
-                countryInfo.insertAdjacentHTML('beforeend', renderCountryList(countries));
+                countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries));
             }
+            renderCountryList(countries);
         })
-        .catch(() => alertBadCountryName())
+        .catch(() => {
+            clearPageHtml();
+            alertBadCountryName();
+        });
 }
         
         
