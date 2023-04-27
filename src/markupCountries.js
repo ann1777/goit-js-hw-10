d5ee546aa36eae89b75d177c596415d59393ed40
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-27 23:07:43 +0300

    Added more styles

--- src/markupCountries.js
+++ src/markupCountries.js
@@ -5,7 +5,7 @@ export function renderCountryList(countries) {
         .map(({ name: { official, common }, flags: { svg } }) => {
             return `
         <li class="country-list__item">
-            <img class="country-list__flag" src="${svg}" alt="Flag of ${common}" width = 25px height = 25px>
+            <img class="country-list__flag" src="${svg}" alt="Flag of ${common}" width = 45px height = 45px>
             <h2 class="country-list__name">${official}</h2>
         </li>
         `
@@ -19,8 +19,8 @@ export function renderCountryInfo(countries) {
         .map(({ flags: { svg }, name, capital, population, languages }) => {
             // languages = Object.values(languages).join(', ');
             return `
-        <img src="${svg}" alt="${name}" width="320" height="auto">
-        <ul class="country-info__list">
+            <ul class="country-info__list">
+            <li><img src="${svg}" alt="${name}" width="320" height="auto"></li>
             <li class="country-info__item"><span class="country-info__item--weight">Capital:</span> ${capital}</li>
             <li class="country-info__item"><span class="country-info__item--weight">Population:</span> ${population}</li>
             <li class="country-info__item"><span class="country-info__item--weight">Languages:</span> ${Object.values(languages)}</li>
