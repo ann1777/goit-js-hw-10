c14e97bf58771a13adfcf5932e2c394a33019ed6
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-27 22:06:21 +0300

    created fetchCountriesInfo function

---
+++ src/fetchCountryInfo.js
@@ -0,0 +1,16 @@
+"use strict"
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const searchParams = 'name, capital, population,flags,languages';
+
+export function fetchCountryInfo (name) {
+    return fetch(`${BASE_URL}${name}?fields=${searchParams}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        return response.json();
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}    
