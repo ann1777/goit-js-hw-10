e67101cc41a742a881122222f9084c8ec8054299
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-28 18:20:34 +0300

    Created addCountriInfo function opened country card onClick by countryName

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ const DEBOUNCE_DELAY = 1300;
 const searchInput = document.querySelector('input#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+const countryListItem = document.querySelector('.country-list_name');
 
 searchInput.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY));
+countryList.addEventListener('click', addCountriInfo);
 
 function onSearchInput(event) {
     event.preventDefault();
@@ -53,4 +55,25 @@ function alertBadCountryName() {
 
 function alertTooManyMatchesFound() {
     Notify.info('Too many matches found. Please enter a more specific name.');
+}
+
+function addCountriInfo({target}) {
+    const contryName = target.textContent;
+    console.log(contryName);
+
+    fetchCountryInfo(contryName)
+        .then(countries => {
+            console.log(countries);
+            for (let i = 0; i < countries.length; i++){
+                console.log(contryName);
+                console.log(countries[i].name);
+            if(contryName === countries[i].name.official) {
+                clearPageHtml();
+
+                console.log("I am here");
+                countryList.insertAdjacentHTML('beforeend', renderCountryList([countries[i]]));
+                countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo([countries[i]]));
+            } 
+        }
+    });
 }
